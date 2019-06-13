b4416e5318a9e9fe3790f240a9860b5011fdfe5a
ndbellew <superdaggler68@gmail.com>
2019-06-13 15:26:26 -0600

    updating Gobbler A few tests to be done still but its getting close.

--- gobbler.py
+++ gobbler.py
@@ -3,27 +3,57 @@
 # Tool that takes in file/folder names, zips them in one temp folder,
 # and sends them to trash.
 
+import sys
 import os
-import optparse import OptionParser
+from optparse import OptionParser
+import zipfile
+from shutil import rmtree
 
-usage = "Usage: %prog [options] [files]"
+
+usage = "Usage: %prog [ -s|--start ][options] [files]"
 parser = OptionParser(usage)
-parser.add_option('-F','--folder', dest='folder',help='If any of the files chosen are/are also folders, then they will be included.')
+parser.add_option('-s','--s',dest='start', help='Select starting location for gobbler to begin gobbling. ex. /home/usr/Documents')
+parser.add_option('-F','--folder', action='store_true',dest='folder',help='If any of the files chosen are/are also folders, then they will be included.')
 parser.add_option('-n', '--name', dest='name',help='Name Zip folder')
-def LinuxMain():
+(options, args)= parser.parse_args()
+
+if len(args) < 1 or options.start is None:
+    parser.print_help()
+    sys.exit(0)
+
+def zipdir(path, ziph):
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            ziph.write(os.path.join(root, file))
+
+def LinuxMain(zipf):
+    path = options.start
+    os.chdir(path)
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if file in args:
+                zipf.write(file)
+                os.remove(file)
+        for dir in dirs:
+            if dir in args and options.folder:
+                zipdir(dir, zipf)
+                rmtree(dir)
+    #zipdir(path, zipf)
+
+def WindowsMain(zipf):
     pass
 
-def WindowsMain():
-    pass
-
-def MacMain():
+def MacMain(zipf):
     pass
 
 if __name__=="__main__":
+    zipf = zipfile.ZipFile('TooBeGobbled.zip', 'w', zipfile.ZIP_DEFLATED)
 
     if os.name == "posix":
-        LinuxMain()
+        LinuxMain(zipf)
     elif os.name == "nt":
         WindowsMain()
     else:
         MacMain()
+
+    zipf.close()
